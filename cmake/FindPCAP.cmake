44c076790d196172260fbe01aad2c09a7b6df0ae
Tobias Mayer <tobim@fastmail.fm>
2019-12-04 19:14:45 +0100

    Use pkg-config for finding PCAP if available

--- cmake/FindPCAP.cmake
+++ cmake/FindPCAP.cmake
@@ -17,66 +17,40 @@
 #  PCAP_LIBRARIES          The PCAP libraries
 #  PCAP_INCLUDE_DIR        The location of PCAP headers
 
-find_path(PCAP_INCLUDE_DIR
-  NAMES pcap.h
-  HINTS ${PCAP_ROOT_DIR}/include)
+find_package(PkgConfig)
 
-find_library(PCAP_LIBRARIES
-  NAMES pcap
-  HINTS ${PCAP_ROOT_DIR}/lib)
-
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(
-  PCAP
-  DEFAULT_MSG
-  PCAP_LIBRARIES
-  PCAP_INCLUDE_DIR)
-
-mark_as_advanced(
-  PCAP_ROOT_DIR
-  PCAP_LIBRARIES
-  PCAP_INCLUDE_DIR)
-
-if (PCAP_FOUND)
-  message(STATUS "Found libpcap: ${PCAP_LIBRARIES}")
-endif ()
-
-# create IMPORTED target for libpcap dependency
-if (PCAP_FOUND AND NOT TARGET pcap::pcap)
-  add_library(pcap::pcap UNKNOWN IMPORTED GLOBAL)
-  set_target_properties(pcap::pcap PROPERTIES
-    IMPORTED_LOCATION ${PCAP_LIBRARIES})
-endif()
-
-# TODO: Replace this with pkg_check_modules from the PkgConfig package, in case
-# we add support for more vendor specific libpcap implementations.
-if (NOT BUILD_SHARED_LIBS)
-  execute_process(COMMAND "/bin/sh" "-c" "nm ${PCAP_LIBRARIES} | grep -q snf_init"
-    RESULT_VARIABLE _result)
-  if (NOT _result)
-    set(PCAP_HAVE_SNF TRUE)
-  else ()
-    set(PCAP_HAVE_SNF FALSE)
+if (PkgConfig_FOUND)
+  pkg_check_modules(PCAP IMPORTED_TARGET libpcap)
+  if (PCAP_FOUND AND NOT TARGET pcap::pcap)
+    add_library(pcap::pcap ALIAS PkgConfig::PCAP)
   endif ()
 endif ()
 
-if (PCAP_HAVE_SNF)
-  if (NOT SNF_ROOT_DIR)
-    set(SNF_ROOT_DIR ${PCAP_ROOT_DIR})
-  endif()
-  find_library(SNF_LIBRARIES
-    NAMES libsnf.a
-    HINTS ${SNF_ROOT_DIR}/lib)
+if (NOT PCAP_FOUND)
+  find_path(PCAP_INCLUDE_DIR
+    NAMES pcap.h
+    HINTS ${PCAP_ROOT_DIR}/include)
 
-  message(STATUS "Found Myricom SNF library: ${SNF_LIBRARIES}")
-  if (SNF_LIBRARIES)
-    if (NOT TARGET aria::snf)
-      add_library(aria::snf STATIC IMPORTED GLOBAL)
-      set_target_properties(aria::snf PROPERTIES
-        IMPORTED_LOCATION ${SNF_LIBRARIES})
-    endif()
+  find_library(PCAP_LIBRARIES
+    NAMES pcap
+    HINTS ${PCAP_ROOT_DIR}/lib)
 
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(
+    PCAP
+    DEFAULT_MSG
+    PCAP_LIBRARIES
+    PCAP_INCLUDE_DIR)
+
+  mark_as_advanced(
+    PCAP_ROOT_DIR
+    PCAP_LIBRARIES
+    PCAP_INCLUDE_DIR)
+
+  # create IMPORTED target for libpcap dependency
+  if (PCAP_FOUND AND NOT TARGET pcap::pcap)
+    add_library(pcap::pcap UNKNOWN IMPORTED GLOBAL)
     set_target_properties(pcap::pcap PROPERTIES
-      INTERFACE_LINK_LIBRARIES "aria::snf")
-  endif()
-endif()
+      IMPORTED_LOCATION ${PCAP_LIBRARIES})
+  endif ()
+endif ()
